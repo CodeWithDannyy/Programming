9125bbacfa108f4196af778514816e093ebaa2db
Daanial Tejani <daanialnuralitejani@gmail.com>
2025-01-28 17:18:37 +0500

    Task 1 Code

--- SEM 2 OOPS/LAB_02/task1.cpp
+++ SEM 2 OOPS/LAB_02/task1.cpp
@@ -3,5 +3,36 @@ using namespace std;
 
 int main()
 {
-    
+    int size;
+
+    cout << "Enter size of an array : ";
+    cin >> size;
+
+    int * arr = new int[size];
+
+    cout << "Enter "<< size <<" Elements in array"<<endl;
+    for(int i = 0; i < size; i++)
+    {
+        cout << "Enter Element "<<i+1<<" : ";
+        cin >> arr[i];
+    }
+
+    int sum, largest = arr[0];
+    float avg;
+    for(int i = 0 ; i < size; i++)
+    {
+        sum += arr[i];
+        if(arr[i] > largest)
+        {
+            largest = arr[i];
+        }
+    }
+    avg = (float)sum/size;
+    cout << "Sum : "<<sum<<endl;
+    cout << "Average : "<<avg<<endl;
+    cout << "Largest Number : "<<largest<<endl;
+
+    delete[] arr;
+
+    return 0;
 }
