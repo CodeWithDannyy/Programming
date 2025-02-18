5e2aec8454d887fd974d09c284043568559ac3e2
Daanial Tejani <daanialnuralitejani@gmail.com>
2025-02-18 18:53:04 +0500

    Update Task2.cpp

--- SEM 2 OOPS/LAB_05/Task2.cpp
+++ SEM 2 OOPS/LAB_05/Task2.cpp
@@ -3,37 +3,42 @@ using namespace std;
 
 class Patient
 {
-private:
-    int ID;
-    string name;
-    string *testResults;
+    private:
+        int ID;
+        string name;
+        string *testResults;
 
-public:
-    Patient(int ID, string name, string *testResults) : ID(ID), name(name), testResults(testResults) {}
+    public:
+        Patient(int ID, string name, string *testResults) : ID(ID), name(name)
+        {
+            this->testResults = new string[5];
+            for (int i = 0; i < 5; i++)
+                this->testResults[i] = testResults[i];
+        }
 
-    Patient(const Patient &obj)
-    {
-        ID = obj.ID;
-        name = obj.name;
-        testResults = new string[5];
-        for (int i = 0; i < 5; i++)
-            testResults[i] = obj.testResults[i];
-    }
+        Patient(const Patient &obj)
+        {
+            ID = obj.ID;
+            name = obj.name;
+            testResults = new string[5];
+            for (int i = 0; i < 5; i++)
+                testResults[i] = obj.testResults[i];
+        }
 
-    void displayInfo()
-    {
-        cout << "ID: " << ID << endl;
-        cout << "Name: " << name << endl;
-        cout << "Test Results: ";
-        for (int i = 0; i < 5; i++)
-            cout << testResults[i] << " ";
-        cout << endl;
-    }
+        void displayInfo()
+        {
+            cout << "ID: " << ID << endl;
+            cout << "Name: " << name << endl;
+            cout << "Test Results: ";
+            for (int i = 0; i < 5; i++)
+                cout << testResults[i] << " ";
+            cout << endl;
+        }
 
-    ~Patient()
-    {
-        delete[] testResults;
-    }
+        ~Patient()
+        {
+            delete[] testResults;
+        }
 };
 
 int main()
